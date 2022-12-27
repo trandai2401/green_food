05de4ae80150ccf43b2f57c1747115e9c0ede829
trandai2401 <trandai@gmail.com>
2022-12-27 11:29:53 +0700

    add data role seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,5 +14,10 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // User::factory(10)->create();
+        $category = new CategorySeeder;
+        $category->run();
+
+        $role = new RoleSeeder;
+        $role->run();
     }
 }
--- database/seeders/RoleSeeder.php
+++ database/seeders/RoleSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Role;
 use Illuminate\Database\Seeder;
 
 class RoleSeeder extends Seeder
@@ -13,6 +14,12 @@ class RoleSeeder extends Seeder
      */
     public function run()
     {
-        //
+        Role::firstOrCreate(['name' => 'ADMIN']);
+
+        Role::firstOrCreate(['name' => 'MANAGER']);
+
+        Role::firstOrCreate(['name' => 'STAFF']);
+
+        Role::firstOrCreate(['name' => 'USER']);
     }
 }
